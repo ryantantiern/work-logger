8a692db3eeb43a880978e24101d7aec737ac8006
ryantantiern <ryantantiern@gmail.com>
2017-12-03 02:03:11 +0000

    format date time to always have 2 digits in each component except year

--- log.py
+++ log.py
@@ -10,17 +10,31 @@ def log():
 	files = [file for file in listdir()]
 	
 	try:
-		date = "{}-{}-{} ".format(date_time_now.year, date_time_now.month, date_time_now.day)
-		time = "{}{}hrs ".format(date_time_now.hour, date_time_now.minute)
-		if date_time_now.hour < 10:
-			time = '0' + time
+		day = date_time_now.day
+		month = date_time_now.month
+		hour = date_time_now.hour
+		minute = date_time_now.minute
+
+		# Ensure there are always 2 digits in these sections
+		if day < 10:
+			day = '0' + str(day)
+		if month < 10:
+			month = '0' + str(month)
+		if hour < 10:
+			hour = '0' + str(hour)
+		if minute < 10:
+			minute = '0' + str(minute)
+
+		date = "{}-{}-{} ".format(date_time_now.year, month, day)
+		time = "{}{}hrs".format(hour, minute)
+
 		text_file = open("{}.txt".format(filename), 'a')
-		text_file.write(date + time + ': ' + message + '\n')
+		text_file.write(date + time + ' : ' + message + '\n')
 		text_file.close()
 		print("Done")
 		return
 	except Exception as e:
-		print('failed writing file', e)	
+		print('failed writing file:', e)	
 		return
 
 
