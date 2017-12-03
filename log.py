e04e936a1bf138003ef1843b477d2df478fed881
ryantantiern <ryantantiern@gmail.com>
2017-12-03 01:48:06 +0000

    finished logging tool

--- log.py
+++ log.py
@@ -1,13 +1,27 @@
-import os
+from os import listdir
+from datetime import datetime as dt
 
 def log():
 	prompt = '>'
-	filename = '_log_notes.txt'
+	filename = '_log_notes'
+	date_time_now = dt.now()
 	message = input(prompt)
-	files = [file for file in os.listdir]
+	files = [file for file in listdir()]
 	
-	if filename in files:
-		try:
+	try:
+		date = "{}-{}-{} ".format(date_time_now.year, date_time_now.month, date_time_now.day)
+		time = "{}{}hrs ".format(date_time_now.hour, date_time_now.minute)
+		if date_time_now.hour < 10:
+			time = '0' + time
+		text_file = open("{}.txt".format(filename), 'a')
+		text_file.write(date + time + ': ' + message + '\n')
+		text_file.close()
+		print("Done")
+		return
+	except Exception as e:
+		print('failed writing file', e)	
+		return
 
-		except Exception as e:
-			print('failed writing file', e)	
+
+log()
+
